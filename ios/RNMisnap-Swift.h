aa53dce12116298f8a2ccf8e7137036f361cd310
Joseph Logan <joseph.ramirez@mawi.io>
2021-11-02 11:23:08 -0600

    Add swift header

---
+++ ios/RNMisnap-Swift.h
@@ -0,0 +1,20 @@
+//
+//  RNMisnap-Swift.h
+//  RNMisnap
+//
+//  Created by Joseph Ramirez on 11/2/21.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#ifndef RNMisnap_Swift_h
+#define RNMisnap_Swift_h
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface MainViewController : UIViewController
+- (MainViewController*) launchButtonAction;
+@end
+
+#endif /* RNMisnap_Swift_h */
